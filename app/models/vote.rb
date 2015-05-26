e4bfcfcc61d84b9e338bce570066d83820787a2e
Joe Reiff <jcreiff@gmail.com>
2015-05-26 14:55:29 -0400

    test for new vote creation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ActiveRecord::Base
   belongs_to :candidate
   belongs_to :voter
 
-  validates :voter_id, uniqueness: true
+  validates :voter_id, presence: true, uniqueness: true
+  validates :candidate_id, presence: true
 
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "votes count" do
+    number_of_votes = Vote.count
+
+    rick = Candidate.create(name: "Rick Boucher", hometown: "Abingdon, VA",
+      district: "VA-9", party: "Democrat")
+    keith = Voter.create(name: "Keith Richards")
+    one = Vote.create(voter_id: keith.id, candidate_id: rick.id)
+
+    assert_equal 1, Vote.count - number_of_votes
+    assert_equal rick, one.candidate
+    assert_equal keith, one.voter
   end
 end
