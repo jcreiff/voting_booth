4548d3df166a1e084000ba5a56bfae596df8f31a
Joe Reiff <jcreiff@gmail.com>
2015-05-26 13:44:24 -0400

    fix validation syntax on Candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,7 +3,9 @@ class Candidate < ActiveRecord::Base
   has_many :votes, dependent: :restrict_with_error
   has_many :voters, through: :votes
 
-  validates :name, presence: true, :hometown, presence: true,
-    :district, presence: true, :party, presence: true
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :district, presence: true
+  validates :party, presence: true
 
 end
