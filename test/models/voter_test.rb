57ed88797d6f1d4c335bce059204ae8ab87d7c45
Joe Reiff <jcreiff@gmail.com>
2015-05-26 15:24:08 -0400

    test association from voter to vote

--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -11,6 +11,17 @@ class VoterTest < ActiveSupport::TestCase
     refute some_rando.save
   end
 
+  test "associated with vote" do
+    mick = Voter.create(name: "Mick Jagger")
+    keith = Voter.create(name: "Keith Richards")
+
+    one = Vote.create(voter: mick, candidate_id: 3)
+    two = Vote.create(voter: keith, candidate_id: 3)
+
+    assert_equal one, mick.vote
+    assert_equal two, keith.vote
+  end
+
   test "associated with candidate" do
     rick = Candidate.create(name: "Rick Boucher", hometown: "Abingdon, VA",
       district: "VA-9", party: "Democrat")
@@ -27,5 +38,4 @@ class VoterTest < ActiveSupport::TestCase
     assert_equal jim, keith.candidate
   end
 
-  
 end
