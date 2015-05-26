fb4ec9602bea579e7a5a7dffa938050bd6448199
Joe Reiff <jcreiff@gmail.com>
2015-05-26 13:56:20 -0400

    test validation for voter name

--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class VoterTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "name validation" do
+    mick = Voter.new(name: "Mick Jagger")
+    keith = Voter.new(name: "Keith Richards")
+    some_rando = Voter.new(name: "")
+
+    assert mick.save
+    assert keith.save
+    refute some_rando.save
   end
 end
