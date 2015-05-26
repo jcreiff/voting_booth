30d6660abb477676e66b7455ddabd5157c989cc3
Joe Reiff <jcreiff@gmail.com>
2015-05-26 13:20:31 -0400

    create model/migration for Candidate, Voter, Vote, and rake

---
+++ db/migrate/20150526171714_create_voters.rb
@@ -0,0 +1,11 @@
+class CreateVoters < ActiveRecord::Migration
+  def change
+    create_table :voters do |t|
+      t.string :name
+      t.string :hometown
+      t.string :party
+
+      t.timestamps null: false
+    end
+  end
+end
