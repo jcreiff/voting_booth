4e3e0728b3fbf64dcf6da9064b2afa1a0c9253a7
Joe Reiff <jcreiff@gmail.com>
2015-05-26 13:52:26 -0400

    test validation of candidate info

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "info validation" do
+    rick = Candidate.new(name: "Rick Boucher", hometown: "Abingdon, VA",
+      district: "VA-9", party: "Democrat")
+    jim = Candidate.new(name: "Jim Jameson", hometown: "Harlan, KY",
+      district: "KY-3")
+    ralph = Candidate.new(name: "Ralph McGill", hometown: "Atlanta, GA",
+      party: "Democrat")
+    hillary = Candidate.new(name: "Hillary Clinton", district: "NY-4",
+      party: "Democrat")
+    some_rando = Candidate.new(hometown: "Lincoln, NE", district: "NE-2",
+      party: "Republican")
+
+    assert rick.save
+    refute jim.save
+    refute ralph.save
+    refute hillary.save
+    refute some_rando.save
   end
 end
