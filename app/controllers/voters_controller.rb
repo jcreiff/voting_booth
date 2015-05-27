dd65b2860cc96f12275610dd71f4d64fd2e83183
Joe Reiff <jcreiff@gmail.com>
2015-05-27 17:16:12 -0400

    fill in voter update and show methods, test

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -10,11 +10,15 @@ class VotersController < ApplicationController
   end
 
   def show
-
+    render json: Voter.find_by_id(params[:id])
   end
 
   def update
-
+    voter = Voter.find_by_id(params[:id])
+    voter.update(name: params[:name]) if params[:name]
+    voter.update(hometown: params[:hometown]) if params[:hometown]
+    voter.update(party: params[:party]) if params[:party]
+    render json: voter
   end
 
 end
--- test/controllers/voters_controller_test.rb
+++ test/controllers/voters_controller_test.rb
@@ -6,4 +6,20 @@ class VotersControllerTest < ActionController::TestCase
       post :create, {name: "Muffin Man", hometown: "Drury, PA"}
     end
   end
+
+  test "can show by id" do
+    tim = Voter.create(name: "Tim Thompson", hometown: "Abingdon, VA")
+    ralph = Voter.create(name: "Ralph Stanley", hometown: "Johnson City, TN")
+
+    get :show, id: tim.id
+    assert response.body =~ /Thompson/
+  end
+
+  test "can update" do
+    tim = Voter.create(name: "Tim Thompson", hometown: "Abingdon, VA")
+    ralph = Voter.create(name: "Ralph Stanley", hometown: "Johnson City, TN")
+
+    patch :update, {id: tim.id, party: "Independent"}
+    assert response.body =~ /Independent/
+  end
 end
