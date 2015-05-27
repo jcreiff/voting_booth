a6f495873794abbd28b782ef72aa3d5581c58601
Joe Reiff <jcreiff@gmail.com>
2015-05-27 13:15:51 -0400

    define index&show for candidate, add show route

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,11 @@
 class CandidatesController < ApplicationController
+
   def index
+    render json: Candidate.all
   end
+
+  def show
+    render json: Candidate.find_by_id(params[:id])
+  end
+
 end
