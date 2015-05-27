057046d96795f07eb22ff58d5397d825b25cb6a9
Joe Reiff <jcreiff@gmail.com>
2015-05-27 13:45:35 -0400

    add create route for candidates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -8,4 +8,14 @@ class CandidatesController < ApplicationController
     render json: Candidate.find_by_id(params[:id])
   end
 
+  def create
+    candidate = Candidate.new(name: params[:name], hometown: params[:hometown],
+        district: params[:district], party: params[:party])
+    if candidate.save
+      render json: candidate
+    else
+      render json: candidate.errors
+    end
+  end
+
 end
--- test/controllers/candidates_controller_test.rb
+++ test/controllers/candidates_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'byebug'
 
 class CandidatesControllerTest < ActionController::TestCase
   test "should get index" do
@@ -6,4 +7,20 @@ class CandidatesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "can create" do
+    assert_difference 'Candidate.count' do
+      post :create, {name: "Will", hometown: "Bel-Air, CA", district: "CA-13",
+      party: "Whigs"}
+    end
+  end
+
+  test "can show by id" do
+    rick = Candidate.create(name: "Rick Boucher", hometown: "Abingdon, VA",
+      district: "VA-9", party: "Democrat")
+    jim = Candidate.create(name: "Jim Jameson", hometown: "Harlan, KY",
+      district: "KY-3", party: "Republican")
+
+    get :show, id: jim.id
+    assert response.body =~ /Jameson/
+  end
 end
